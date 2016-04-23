2ee1aacdf1998e6c502efc5746db4cece4dcbf5b
BlackChandelier <budaibori95@gmail.com>
2016-04-23 17:25:08 +0200

    csv beolvasás

--- Main.java
+++ Main.java
@@ -1,14 +1,24 @@
 package projlab;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 		
-	public static void main(String[] args) throws IOException {
-	    Scanner scanner = new Scanner(System.in);
-		System.out.print("Ez a pályád: *pálya*\n");
-		System.out.print("Adj meg egy parancsot: ");
+	public static void main(String[] args) throws IOException, FileNotFoundException{	   
+	    System.out.println("Ez a pályád:\n");
+	    
+	    Scanner scanner = new Scanner(new File("level1.csv"));
+        scanner.useDelimiter(",");
+        while(scanner.hasNext()){
+            System.out.print(scanner.next()+" "); 
+           
+        }
+        scanner.close();
+
+		System.out.print("\n\nAdj meg egy parancsot: ");
 
 	}
 }
